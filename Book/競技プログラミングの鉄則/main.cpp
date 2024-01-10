255f3569fba85ae2a244566c9e2a27d018080083
humiki-kiroa <h.kimura.github@gmail.com>
2024-01-10 09:58:31 +0900

    update

--- Book/競技プログラミングの鉄則/main.cpp
+++ Book/競技プログラミングの鉄則/main.cpp
@@ -1,52 +1,21 @@
 #include <bits/stdc++.h>
 
-ulong N,K;
+long N,K;
 using namespace std;
 int main(){
     cin >> N >> K;
 
-    vector<ulong> A(N,0);
-    for(ulong i = 0; i < N; i ++) cin >> A[i];
+    long sub = K - (N - 1) * 2;
 
-
-    // step1:calc Afront sum & Aback sum
-    ulong element_size = N / 2;
-    vector<ulong> AFsum(1 << element_size,0);
-    ulong pattern = (1 << element_size);
-
-    for(ulong i = 0; i < pattern; i ++){
-        ulong ans = 0;
-
-        for(ulong j = 0; j < element_size; j ++){
-            ulong div = (i / (1 << j));
-            if(div % 2 == 1) ans += A[j];
-        }
-        AFsum[i] = ans;
+    if(sub < 0){
+        cout << "No" << endl;
+        return 0;
     }
 
-    vector<ulong> ABsum(1 << (N - element_size),0);
-    pattern = (1 << (N - element_size));
-
-    for(ulong i = 0; i < pattern; i ++){
-        ulong ans = 0;
-
-        for(ulong j = element_size; j < N; j ++){
-            ulong div = ((i << element_size) / (1 << j));
-            if(div % 2 == 1) ans += A[j];
-        }
-        ABsum[i] = ans;
+    if(sub % 2 == 0){
+        cout << "Yes" << endl; 
     }
+    else cout << "No" << endl;
 
-    sort(ABsum.begin(),ABsum.end());
-
-    // step2 片方の要素を引いた値を2分探索
-    for(ulong i = 0; i < (1 << element_size); i ++){
-        ulong ans = K - AFsum[i];
-        if(binary_search(ABsum.begin(),ABsum.end(),ans)){
-            cout << "Yes" << endl;
-            return 0;
-        }
-    }
-    cout << "No" << endl;
     return 0;
 }
